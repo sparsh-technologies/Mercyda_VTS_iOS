f329889c6b48e041b2554ea60531829fc81147f5
Paul Jose <p.jose@jaiecom.com>
2020-06-30 01:12:47 +0300

    Commented Crash in ViewDidLoad

--- MERCYDA TRACK/ViewController.swift
+++ MERCYDA TRACK/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //self.callDemoWebservice()
-        self.crash()
+        //self.crash()
        
     }
     
