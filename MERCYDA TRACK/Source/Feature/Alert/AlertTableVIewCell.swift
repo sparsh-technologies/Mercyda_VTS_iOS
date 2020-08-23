ca455e2a36c3c680fbaea93156854043009f6b9b
shaiju <shaijumathew15@gmail.com>
2020-08-23 22:10:08 +0530

    alert stack view

--- MERCYDA TRACK/Source/Feature/Alert/AlertTableVIewCell.swift
+++ MERCYDA TRACK/Source/Feature/Alert/AlertTableVIewCell.swift
@@ -6,7 +6,7 @@
 import UIKit
 
 class AlertTableVIewCell: UITableViewCell {
-
+    
     @IBOutlet weak var vehicleNumberLabel: UILabel!
     @IBOutlet weak var vehicleType: UIImageView!
     @IBOutlet weak var alertDispalayView: UIView!
@@ -16,15 +16,19 @@ class AlertTableVIewCell: UITableViewCell {
     @IBOutlet weak var emergencyAlertView: UIView!
     @IBOutlet weak var overspeedAlertView: UIView!
     
+    @IBOutlet weak var mainPowerAlertlabel: UILabel!
+    @IBOutlet weak var wirecutAlertLabel: UILabel!
+    @IBOutlet weak var emergencyAlertLabel: UILabel!
+    @IBOutlet weak var overspeedAlertLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     override func layoutSubviews() {
@@ -39,46 +43,49 @@ class AlertTableVIewCell: UITableViewCell {
             alertCountLabel.text = "\(alertCOunt)"
         }
         
-   //    mainpowerOffView.isHidden = true
+        //mainpowerOffView.isHidden = true
         
-//        if let mainpowerRemoval = vehicleObject.main_power_removal_alert_count{
-//            if mainpowerRemoval == 0{
-//                mainpowerOffView.isHidden = true
-//            }
-//        }
+        if let mainpowerRemoval = vehicleObject.main_power_removal_alert_count{
+            
+            mainpowerOffView.isHidden = mainpowerRemoval == 0
+            mainPowerAlertlabel.text = "\(mainpowerRemoval)"
+        }
         
-//        if let wirecutAlert = vehicleObject.wire_cut_alert_count{
-//                   if wirecutAlert == 0{
-//                       wireCutAlertView.isHidden = true
-//                   }
-//               }
-//
-//        if let emergencyAlert = vehicleObject.emergency_alert_count{
-//                          if emergencyAlert == 0{
-//                              emergencyAlertView.isHidden = true
-//                          }
-//                      }
-//
-//
-//               if let overspeed = vehicleObject.emergency_alert_count{
-//                                 if overspeed == 0{
-//                                     overspeedAlertView.isHidden = true
-//                                 }
-//                             }
+        if let wirecutAlert = vehicleObject.wire_cut_alert_count{
+            wireCutAlertView.isHidden = wirecutAlert == 0
+            wirecutAlertLabel.text = "\(wirecutAlert)"
+            
+        }
+        
+        if let emergencyAlert = vehicleObject.emergency_alert_count{
+            emergencyAlertView.isHidden = emergencyAlert == 0
+            emergencyAlertLabel.text = "\(emergencyAlert)"
+        }
+        
+        
+        if let overspeed = vehicleObject.emergency_alert_count{
+            
+            overspeedAlertView.isHidden = overspeed == 0
+            printLog(overspeed)
+            overspeedAlertLabel.text = "\(overspeed)"
+        }
+        DispatchQueue.main.async {
+            self.layoutIfNeeded()
+        }
         
     }
     
     
     func getVehicleType(type:String){
-          switch type{
-          case VehicleModel.Lorry.rawValue:
-              self.vehicleType.image = UIImage.init(named:"Lorry")
-          case VehicleModel.MiniTruck.rawValue:
-              self.vehicleType.image = UIImage.init(named: "minilorry")
-          case VehicleModel.Car.rawValue:
-              self.vehicleType.image = UIImage.init(named: "car")
-          default:
-              self.vehicleType.image = UIImage.init(named:"Lorry")
-          }
-      }
+        switch type{
+        case VehicleModel.Lorry.rawValue:
+            self.vehicleType.image = UIImage.init(named:"Lorry")
+        case VehicleModel.MiniTruck.rawValue:
+            self.vehicleType.image = UIImage.init(named: "minilorry")
+        case VehicleModel.Car.rawValue:
+            self.vehicleType.image = UIImage.init(named: "car")
+        default:
+            self.vehicleType.image = UIImage.init(named:"Lorry")
+        }
+    }
 }
